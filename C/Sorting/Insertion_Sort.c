08f8ccbf5d91466b0b33ec76f907427e1a8a9e3c
kodosuke <ajaykrishnaanandhan@yahoo.com>
2021-07-31 10:54:27 +0530

    Begin - Insertion Sort

---
+++ C/Sorting/Insertion_Sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+
+    int nums[] = {10, 9, 7, 101, 23, 44, 12, 78, 34, 23}; 
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+
+
+    return 0;
+}
+
+int *insertionSort(int nums[], int size) {
+
+
+}
+
