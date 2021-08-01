827d0775785a8e7c0746bd0a3d0c6e6d984b4899
kodosuke <ajaykrishnaanandhan@yahoo.com>
2021-08-01 12:07:22 +0530

    Quick Sort

--- C/Sorting/Quick_Sort.c
+++ C/Sorting/Quick_Sort.c
@@ -1,58 +1,12 @@
 # include <stdio.h>
 # include <conio.h>
 
-
-void swap( int *m, int *n)
-{
-    int t;
-
-     t = *m;
-    *m = *n;
-    *n = t;
-
-}
+void swap( int *m, int *n);
+int partition( int array[], int left, int right );
+void quickSort(int array[], int left, int right);
+void print(int array[], int size);
 
 
-int partition( int array[], int left, int right )
-{
-    int pivot = array[ right ];
-
-    int i = left - 1;
-
-    for ( int j = left; j < right; j++ )
-    {
-        if ( array[ j ] <= pivot )
-        {
-            i ++ ;
-            swap(&array[i], &array[j]);
-        }
-    } 
-    swap(&array[ i + 1 ], &array[ right ]);
-
-    return i + 1;
-}
-
-
-void quickSort(int array[], int left, int right)
-{
-    if( left < right )
-    {
-        int pivotIndex = partition( array, left, right );
-
-        quickSort( array, left, pivotIndex - 1  );
-        quickSort( array, pivotIndex + 1, right );
-    }
-}
-
-// Print
-void print(int array[], int size) {
-  for (int i = 0; i < size; ++i) {
-    printf("%d  ", array[i]);
-  }
-  printf("\n");
-}
-
-// Driver Code
 int main()
 {
     int array[] = {8, 7, 2, 1, 0, 9, 6};
@@ -69,3 +23,54 @@ int main()
 
     return 0;
 }
+
+
+void swap( int *m, int *n) {
+
+    int t;
+
+     t = *m;
+    *m = *n;
+    *n = t;
+}
+
+
+int partition( int array[], int left, int right ) {
+
+    int pivot = array[ right ];
+
+    int i = left - 1;
+
+    for ( int j = left; j < right; j++ ) {
+
+        if ( array[ j ] <= pivot ) {
+
+            i ++ ;
+            swap(&array[i], &array[j]);
+        }
+    } 
+    swap(&array[ i + 1 ], &array[ right ]);
+
+    return i + 1;
+}
+
+
+void quickSort(int array[], int left, int right) {
+
+    if( left < right ) {
+
+        int pivotIndex = partition( array, left, right );
+
+        quickSort( array, left, pivotIndex - 1  );
+        quickSort( array, pivotIndex + 1, right );
+    }
+}
+
+
+void print(int array[], int size) {
+
+  for (int i = 0; i < size; ++i)   printf("%d  ", array[i]);
+  
+  printf("\n");
+}
+
